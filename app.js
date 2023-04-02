7dbdd6d3b07bf013ae07c42053e6f93eff78171d
Pravesh <pritamyadav70923@gmail.com>
2023-04-02 14:33:39 +0530

    learnings from lesson 4 till mock server

--- app.js
+++ app.js
@@ -11,8 +11,34 @@ var submitButton= document.getElementById("submit");
 var textInput=document.getElementById("enterYourMsg");
 
 
+// var value="";
+// submitButton.addEventListener("click",function(){
+//      value+=textInput.value;
+//     console.log(value);
+// })
 
+// live excersixe.....
+console.log(document.querySelector("textarea"));
+
+console.log(document.querySelector(".btn-primary"));
+
+console.log(document.getElementById("submit"));
+
+console.log(document.querySelector("[name='userMsg']"))
+
+var out =document.getElementById("out");
+//endpoint for serverCALL
+var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+//function to make a path with parameters
+
+function getTranslationURL(input){
+   return serverURL+"?"+"text="+input;
+}
 submitButton.addEventListener("click",function(){
-    var value=textInput.value;
-    console.log(value);
+    var inputText=textInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(respose => respose.json())
+    .then(json =>console.log(json.contents.translated));
 })
+
